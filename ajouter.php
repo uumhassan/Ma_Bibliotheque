d3479adb134756c4d285efb322cd6302c7ec61c9
Uumhassan <foulaye.sidibe@gmail.com>
2025-04-17 14:31:47 +0200

    Modification formulaire ajout d'un livre

--- ajouter.php
+++ ajouter.php
@@ -1,19 +1,25 @@
-<?php require_once 'includes/connexion.php'; ?>
+<!DOCTYPE html>
+<html>
 
-<h2>Ajouter un livre</h2>
-<form method="post" action="traitement_ajout.php">
-    Titre : <input type="text" name="titre" required><br>
-    Année : <input type="number" name="annee" required><br>
-    Auteur :
-    <select name="id_auteur" required>
-        <?php
-        $auteurs = $pdo->query("SELECT * FROM auteurs");
-        foreach ($auteurs as $auteur) {
-            echo '<option value="' . $auteur['id_auteur'] . '">';
-            echo htmlspecialchars($auteur['prenom'] . ' ' . $auteur['nom']);
-            echo '</option>';
-        }
-        ?>
-    </select><br>
-    <input type="submit" value="Ajouter">
-</form>
+<head>
+    <title>Ajouter un livre</title>
+</head>
+
+<body>
+    <h2>Ajouter un livre</h2>
+    <form method="post" action="traitement_ajout.php">
+        Titre: <input type="text" name="titre" required><br>
+        Année: <input type="number" name="annee" required><br>
+        Auteur:
+        <select name="id_auteur" required>
+            <?php foreach ($auteurs as $auteur): ?>
+                <option value="<?= $auteur['id_auteur'] ?>">
+                    <?= htmlspecialchars($auteur['prenom'] . ' ' . $auteur['nom']) ?>
+                </option>
+            <?php endforeach; ?>
+        </select><br>
+        <input type="submit" value="Ajouter">
+    </form>
+</body>
+
+</html>
